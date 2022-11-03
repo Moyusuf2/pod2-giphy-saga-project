10ab510affac79cccf2f835d685f45a5083d75cd
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-11-03 14:42:08 -0500

    added giphy router GET

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const pool = require('../modules/pool');
-
 const router = express.Router();
+const axios = require('axios');
 
-// return all giphy results
 router.get('/', (req, res) => {
-    res.sendStatus(200);
-});
+    axios({
+        method: 'GET',
+        url: 'https://api.giphy.com/v1/gifs/search',
+        params: {
+            api_key:process.env.GIPHI_API_KEY,
+            q:req.body
+        }
+    })
+    .then((resAPI)=>{
+        res.send(resAPI.data);
+    })
+    .catch((err)=>{
+        console.error('error in search router axios GET',err);
+        res.sendStatus(500);
+    })
+})
 
 module.exports = router;
