b9443cc99989274dd140a0203dfb1b999123ff78
Daniel Legan <legan.daniel@gmail.com>
2022-11-04 11:31:19 -0500

    able to update category of individual favorite gifs

--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,20 +1,72 @@
-function FavoriteItem({ element, categoryList }) {
-    // button onClick ... dispatch with SOMETHING, payload id
-    // in index, put route to change with payload
-    function updateCategory(id) {
-        console.log('in updateCategory with category id', id);
-        // dispatch
+import { useDispatch, useSelector } from 'react-redux';
+
+function FavoriteItem({ element }) {
+
+    const dispatch = useDispatch();
+
+    const categoryName = {
+        '1': 'Funny',
+        '2': 'Cohort',
+        '3': 'Cartoon',
+        '4': 'NSFW',
+        '5': 'Meme',
+        'null': 'None'
     }
 
     return (
         <div className="favoriteItem">
             <img src={element.url} />
-            <div>{element.categoyry_id}</div>
-            <button onClick={() => { updateCategory(categoryList.id) }}>Funny</button>
-            <button>Cohort</button>
-            <button>Cartoon</button>
-            <button>NSFW</button>
-            <button>Meme</button>
+            {/* obj[something] === obj.something */}
+            <div>Favorite Category: {categoryName[element.categoyry_id]}</div>
+            <div>Favorite ID: {element.id}</div>
+            <button onClick={() => dispatch(
+                {
+                    type: 'UPDATE_CATEGORY',
+                    payload: {
+                        id: element.id,
+                        data: 1
+                    }
+                })}>Funny</button>
+            <button onClick={() => dispatch(
+                {
+                    type: 'UPDATE_CATEGORY',
+                    payload: {
+                        id: element.id,
+                        data: 2
+                    }
+                })}>Cohort</button>
+            <button onClick={() => dispatch(
+                {
+                    type: 'UPDATE_CATEGORY',
+                    payload: {
+                        id: element.id,
+                        data: 3
+                    }
+                })}>Cartoon</button>
+            <button onClick={() => dispatch(
+                {
+                    type: 'UPDATE_CATEGORY',
+                    payload: {
+                        id: element.id,
+                        data: 4
+                    }
+                })}>NSFW</button>
+            <button onClick={() => dispatch(
+                {
+                    type: 'UPDATE_CATEGORY',
+                    payload: {
+                        id: element.id,
+                        data: 5
+                    }
+                })}>Meme</button>
+            <button onClick={() => dispatch(
+                {
+                    type: 'UPDATE_CATEGORY',
+                    payload: {
+                        id: element.id,
+                        data: null
+                    }
+                })}>None</button>
         </div>
     );
 }
